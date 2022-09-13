c2b26ee7fd706172403526bac238441109297ba5
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-09-13 23:16:50 +0300

    add final version of ex6

--- pattern_unlock_6/src/main/java/Level1.java
+++ pattern_unlock_6/src/main/java/Level1.java
@@ -1,34 +1,28 @@
-public class Test {
-    public static void main(String[] args) {
-        double diag = (float) Math.sqrt(2); //расчет диагонали - 1.41..
-        //double sum = diag * 2 + 7; //число-сумма например 9.828427
+public class Level1 {
+    public static String PatternUnlock(int N, int[] hits) {
+        double diag = (float) Math.sqrt(2);
         double sum = 0;
-        //(int)Math.round(sum * 100000) - мы результат с кучей знаков умнож. на 100000 - чтобы знаки убрать
-        //Math.round() - округлили полученный ранее результат
-        //Integer.toString - перевели в строку число. .replace - сделали замену.
 
-        sum=0;
-        int[] hits = {1,2,3,4,5,6,2,7,8,9};
-        for(int i=0; i<hits.length-1; i++) {
+        for(int i=0; i<(N-1); i++) {
             switch (hits[i]) {
                 case 1:
                     switch (hits[i+1]) {
                         case 6, 2, 9:
-                          sum++;
-                          break;
+                            sum++;
+                            break;
                         case 5, 8:
-                          sum+=diag;
-                          break;
+                            sum+=diag;
+                            break;
                         default: sum+=0;
                     } break;
                 case 2:
                     switch (hits[i+1]) {
                         case 1, 3, 5, 8:
-                           sum++;
-                           break;
+                            sum++;
+                            break;
                         case 6, 9, 7, 4:
-                           sum+=diag;
-                           break;
+                            sum+=diag;
+                            break;
                         default: sum+=0;
                     } break;
                 case 3:
@@ -104,12 +98,8 @@ public class Test {
             }
         }
         int res = Integer.parseInt(Integer.toString((int)Math.round(sum * 100000))
-                .replace("0", "")); //перевод в нужный формат 9.828427 -> 982843
+                .replace("0", ""));
 
-        System.out.println(Integer.toString(res));
-
-        //ПРОВЕРКА
-        int[] hits1 = {1,2,3,4,5,6,2,7,8,9};
-        System.out.println("строка - " + Level1.PatternUnlock(10, hits1));
+        return Integer.toString(res);
     }
 }
