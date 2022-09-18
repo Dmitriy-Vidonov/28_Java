7bc93575f1c1aa9e6ef786d26cd8149b8e845759
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-09-18 15:39:53 +0300

    add encode matrix

--- Red_Case_9/src/main/java/Test.java
+++ Red_Case_9/src/main/java/Test.java
@@ -1,17 +1,19 @@
 public class Test {
     public static void main(String[] args) {
-        String str = "отдай мою кроличью лапку сука";
+        String str = "отдай мою кроличью лапку";
         str = str.replace(" ", ""); //убрали пробелы
         int len = str.length();
         int rows = (int) Math.sqrt(len); //число строк матрицы
         int cols = (int) Math.ceil(Math.sqrt(len)); //число столбцов матрицы
         if(rows * cols < len) rows++;
 
+        //рассчет diff только в случае зашифровки boolean encode = true
         int diff = 0; //дополняем строку пробелами, если она не полностью входит в матрицу
         if(str.length() < cols*rows){
             diff = cols*rows-str.length();
         }
 
+        //в случае encode = true
         for(int k=0; k<diff; k++) {
             str = str + " ";
         }
@@ -19,6 +21,8 @@ public class Test {
 
         char[][] matrix = new char[rows][cols]; //[5][5]
         int pointer=0;
+
+        //в случае encode = true
         for(int i=0; i<rows; i++) { //цикл по строкам
             for(int j=0; j<cols; j++) {
                 matrix[i][j] = str.charAt(pointer);
@@ -32,10 +36,11 @@ public class Test {
             }
             System.out.println();
         }
-        str = "";
+        //encode = true
+        str = ""; //обнуляем строку перед заполнением из матрицы
         for(int c=0; c<cols; c++) {
             for(int d=0; d<rows; d++) {
-                str += String.valueOf(matrix[d][c]); //помещаем в строку инветированный вывод из массива
+                str += String.valueOf(matrix[d][c]); //помещаем в строку инвертированный вывод из массива
             }
             str += " "; //ставим пробел после считывания каждого столбца
         }
@@ -43,8 +48,42 @@ public class Test {
         str = str.substring(0, str.length()-1); //срезали последний пробел
 
         System.out.println(str);
-        System.out.println(str.length());
 
+        String strEncode = str; //на расшифровку даем строку, полученную в ходе зашифровки
+        //strEncode = strEncode.replace(" ", "");
+
+        boolean encode = false; //флажок на расшифровку
+        //rows и cols у нас известны, матрица тех же размеров
+        char[][] matrixEncode = new char[rows][cols]; //[5][5]
+        int pointerEncode=0;
+
+        int i;
+        for(int j=0; j<cols; j++) {
+            i=0;
+            while (i<rows && pointerEncode<strEncode.length()){
+                if(i < rows-1) {
+                    matrixEncode[i][j] = strEncode.charAt(pointerEncode);
+                    pointerEncode++;
+                    i++;
+                } else { //если последний элемент, который надо вставить в очередном шаге цикла...
+                    if (strEncode.charAt(i) != ' ') { //...не равен пробелу, то идем дальше как обычно
+                        continue;
+                    } else pointerEncode++; //если последний элемент равен пробелу, значит нам надо перескочить по строке
+                    break; //и завершить цикл
+                }
+
+           }
+        }
+        //тестовый вывод матрицы
+        for(int i1=0; i1<rows; i1++) {
+            for(int j1=0; j1<rows; j1++) {
+                System.out.print(matrixEncode[i1][j1]);
+            }
+            System.out.println();
+        }
+
+        //считываем матрицу в строку
+        strEncode = ""; //обнуляем итоговую строку дешифровки перед занесением данных из матрицы
         
     }
 }
