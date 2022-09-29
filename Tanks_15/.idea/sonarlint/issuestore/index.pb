185418b5073ee7d8fa700627f305cc2a561b7125
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-09-29 08:32:07 +0300

    creating check method for subarray in progress

---
+++ Tanks_15/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+X
+(src/main/java/Test_break_with_label.java,7/a/7a981f109ae15a8b4118f56117c740b41d26adab
+W
+'src/main/java/Test_continue_simple.java,2/f/2fba9e516ef14459fb50bf6fbe28a0ed444abdea
+X
+(src/main/java/Test_continue_w_label.java,8/5/8529aed12ccea41a29369517c0551aa2c865f6f4
+N
+src/main/java/Test_return.java,f/5/f5492c2e43fa7d3224a486133ad0988dbd5f0f2e
+e
+5src/main/java/Test_break_w_label_inner_loops_out.java,4/a/4ab6585dd9626317578e01acffa9522d42241477
--- Tanks_15/src/main/java/Test.java
+++ Tanks_15/src/main/java/Test.java
@@ -17,6 +17,37 @@ public class Test {
         System.out.println();
         Methods.ArrShow(arr2);
 
+        System.out.println();
 
+        boolean t = false;
+        //нужен метод, который по начальным координатам в массиве проверит вхождение в него подмассива
+        //проход по массиву, увидели совпадение с первым элементом подмассива - запустили проверку.
+        //вернуло - false - продолжаем. вернуло - true - завершаем все
+        mainloop: for (int rows=0; rows<H1; rows++) { //цикл по строкам arr1
+            for (int cols=0; cols<W1; cols++) { //цикл по столбцам arr1
+                if (arr1[rows][cols] == arr2[0][0]) {
+                    //вызов метода проверки
+                    //if (Methods.SunArrayCheck(arr1[][], arr2[][], cols, rows) == true) break mainloop;
+                    if (t) {
+                        break mainloop;
+                    } //else System.out.print(arr1[rows][cols] + " "); //если else continue, то мы не пропечатаем совпадение
+                } else System.out.print(arr1[rows][cols] + " ");
+            }
+            System.out.println();
+        }
+        //метод проверки - пока пропишем руками идеальный вариант развития событий. Затем доьавим ограничители
+        int cols = 1;
+        int rows = 1;
+        int arrEqualsCounter = 0;
+        int arr2Cols = 0; //для координат arr2 - столбцы
+        int arr2Rows = 0; //для координат arr2 - строки
+        for (int i=rows; i<rows+arr2.length-1; i++) { //цикл по строкам
+            for (int j=cols; j<cols+arr2[0].length-1; j++) {
+                if (arr1[i][j] == arr2[arr2Rows][arr2Cols]) arrEqualsCounter++;
+                arr2Cols++;
+            }
+            arr2Rows++;
+        }
+        System.out.println("число совпадений = " + arrEqualsCounter);
     }
 }
