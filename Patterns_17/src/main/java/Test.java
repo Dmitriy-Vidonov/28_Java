20257f570c155010f1348481a0c6d5f49a3ae974
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-10-04 21:59:58 +0300

    update draft method.

--- Patterns_17/src/main/java/Test.java
+++ Patterns_17/src/main/java/Test.java
@@ -1,12 +1,14 @@
 public class Test {
     public static void main(String[] args) {
-        String str = "*..*..*..*..*..*..*";
+        String str = "**";
         char[] chArr = str.toCharArray();
         //for(char letter : chArr) System.out.print(letter + " ");
         int stars = 1;
         int dots = 0;
         int index = 1;
-        while(chArr[index] != '*') {
+
+        //осталось доработать лишь эту проверку. Она должна учитывать, что вся строка может быть звездочками
+        while(chArr[index] != '*' && chArr.length > 1) {
             dots++;
             index++;
         } //dots = 2, stars = 1
@@ -21,5 +23,27 @@ public class Test {
         System.out.println(str.substring(0, (dots + stars)).equals(subStr)); //true
 
         //далее просто надо сдвигаться и дальше проверять - до предпоследнего символа, который должен быть *
+        int StrPartStart = 0; //начало куска строки для поиска подстроки
+        int StrPartEnd = dots + stars; //конец куска строки для поиска подстроки. Равен длине шаблончика - 3
+
+        //Проверка корректности длины строки
+        if ((chArr.length-1) % (dots + stars) == 0) System.out.println("Длина корректна!");
+        else System.out.println("Длина некорректна!");
+
+        int checks = 0;
+        for (int i = 0; i <= chArr.length-1 && i + (dots + stars) <= chArr.length-1
+                && chArr.length >= (dots + stars) && chArr[chArr.length-1] == '*'
+                && chArr[0] == '*'; i += (dots + stars)) {
+            //System.out.println("i = " + i);
+            if (str.substring(i, i + (dots + stars)).equals(subStr) == true) {
+                System.out.println("Проверка пройдена!");
+                checks++;
+            } else {
+                System.out.println("Проверка не пройдена!");
+                break;
+            }
+        }
+        if (checks == (chArr.length-1) / (dots + stars)) System.out.println("Все проверки пройдены!");
+        else System.out.println("В строке найдена ошибка!");
     }
 }
