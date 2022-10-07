d5fda5615563e8dc08d9abc97e7e0d0d53a00108
Dmitriy-Vidonov <73774560+Dmitriy-Vidonov@users.noreply.github.com>
2022-10-07 15:28:01 +0300

    Update Level1.java

--- OLAP_Shop_19/src/main/java/Level1.java
+++ OLAP_Shop_19/src/main/java/Level1.java
@@ -1,21 +1,35 @@
 import java.util.*;
+
 public class Level1 {
     public static String[] ShopOLAP(int N, String []items){
         int buffer = 0;
-        Map<String, Integer> testMap2 = new TreeMap<String, Integer>();
+        Map<String, Integer> testMap = new TreeMap<String, Integer>();
 
         for(int i=0; i<N; i++) {
             String[] strElement = items[i].split(" ");
-            if(testMap2.containsKey(strElement[0])) {
-                buffer = testMap2.get(strElement[0]);
-                testMap2.put(strElement[0], (Integer.parseInt(strElement[1]) + buffer));
-            } else testMap2.put(strElement[0], Integer.parseInt(strElement[1]));
+            if(testMap.containsKey(strElement[0])) {
+                buffer = testMap.get(strElement[0]);
+                testMap.put(strElement[0], (Integer.parseInt(strElement[1]) + buffer));
+            } else testMap.put(strElement[0], Integer.parseInt(strElement[1]));
         }
-        String resultStr = testMap2.toString();
+        testMap = sortByValue(testMap);
+        String resultStr = testMap.toString();
         resultStr = resultStr.replaceAll("[{}]", "");
         resultStr = resultStr.replaceAll("=", " ");
         String[] resArray = resultStr.split(", ");
 
         return resArray;
+    }
+    
+    private static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        return map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+                .collect(java.util.stream.Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
     }
 }
