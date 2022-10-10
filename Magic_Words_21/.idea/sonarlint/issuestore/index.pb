2ac82d17a7b82f1f9433e61bfab172c75dac85cb
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-10-10 08:29:29 +0300

    some thoughts about str replace

--- Magic_Words_21/.idea/sonarlint/issuestore/index.pb
+++ Magic_Words_21/.idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,3 @@
 
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
-G
-src/main/java/Test.java,b/6/b660a5f333c00ef78dec1d85c727eb000157c1f3
-H
-src/main/java/Test2.java,f/5/f5904f6d4ac127cbd673aea301c76c2f5b9b576b
--- Magic_Words_21/src/main/java/Test2.java
+++ Magic_Words_21/src/main/java/Test2.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Test2 {
     public static void main(String[] args) {
         String str = "abлм";
@@ -5,7 +7,27 @@ public class Test2 {
         System.out.println("b = " + (int)str.charAt(1));
         System.out.println("л = " + (int)str.charAt(2));
         System.out.println("м = " + (int)str.charAt(3));
+        int i, j; i=j=0;
+        String strReplace = "нкмл";
+        char[] strToArr = strReplace.toCharArray();
+        String bufferStr = ""; //буферная строка, для хранения промежуточных значений
+        ArrayList<String> bufferArray = new ArrayList<String>(); //массив, в который будем заносить полученные перестановки
+        for(i=0; i<strReplace.length(); i++) { //основной цикл по каждой букве
+            for(j=0; j<strReplace.length(); j++) { //в этом цикле будем сдвигать буквы
+                if (strReplace.charAt(i) == strReplace.charAt(j)) continue;
+                else {
+                    //поменять местами символы в массиве, потом полученное записать в строку
+                    char temp;
+                    temp = strToArr[i];
+                    strToArr[i] = strToArr[j];
+                    strToArr[j] = temp;
 
-
+                    bufferStr = strToArr.toString();
+                    bufferArray.add(bufferStr);
+                }
+            }
+        }
+        //тестовый вывод полученного буферного массива
+        for(String word : bufferArray) System.out.print(word + " ");
     }
 }
