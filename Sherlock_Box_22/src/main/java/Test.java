fe76cd5219a469942f7934300a5f58a372287dc2
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-10-13 22:25:38 +0300

    draft method is ready

--- Sherlock_Box_22/src/main/java/Test.java
+++ Sherlock_Box_22/src/main/java/Test.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        String str = "XXYYZA";
+        String str = "XXYYZABC";
         //Перевели строку в ArrayList
         ArrayList<Character> strArray = new ArrayList<>();
         for(int i=0; i<str.length(); i++) {
@@ -15,7 +15,6 @@ public class Test {
         strArray.addAll(set); //заполнили заново уже упорядоченным списком
 
         List<Integer> nums = new ArrayList<>(); //список для хранения частотности букв
-
         for(char word : strArray) {
             int buff_count = 0;
             for(int i = 0; i < str.length(); i++) {
@@ -25,23 +24,25 @@ public class Test {
             }
             nums.add(buff_count);
         }
+
+        Collections.sort(nums);
         System.out.print("массив чисел - ");
         for(int digit : nums) System.out.print(digit + ", "); System.out.println();
 
-        //обработать массив чисел
-        int mainDigit = nums.get(0);
+        int minEl = Collections.min(nums);
+        int maxEl = Collections.max(nums);
+        int minCount = 0;
+        int maxCount = 0;
+        for(int digit : nums) {
+            if(digit == minEl) minCount++;
+            else if (digit == maxEl) maxCount++;
+            else continue;
+        }
+        System.out.println("число максимальных = " + maxCount);
+        System.out.println("число минимальных = " + minCount);
 
-        Set<Integer> set2 = new HashSet<>(nums); //создали set на основе ранее созданной коллекции
-        nums.clear(); //очистили изначальную коллекцию
-        nums.addAll(set2); //заполнили заново уже упорядоченным списком
-
-        System.out.print("массив чисел2 - ");
-        for(int digit : nums) System.out.print(digit + ", "); System.out.println();
-
-        //надо обрабатывать массив типа 1,2,2,1 а не конечный 1,2. 
-        if(nums.size() > 2) System.out.println("не валидно");
-        else if(nums.size() == 2 && ((nums.get(0) - nums.get(1)) == (1 | -1))) System.out.println("валидно");
-        else if(nums.size() == 1) System.out.println("валидно");
-        else System.out.println("не валидно");
+        if((maxCount > 0 && (((maxEl - minEl) == 1) && (maxCount == 1 || minCount == 1))) || maxCount == 0) {
+            System.out.println("валидно");
+        } else System.out.println("не валидно");
     }
 }
