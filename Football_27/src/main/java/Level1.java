e817a2beba30bb29c6ab3e5108450c0fa6975bac
Dmitrii Vidonov <bankarmy@yandex.ru>
2022-10-21 23:21:54 +0300

    the first method in draft for ex27 is work

--- Football_27/src/main/java/Level1.java
+++ Football_27/src/main/java/Level1.java
@@ -8,11 +8,13 @@ public class Level1 {
         boolean result = false;
 
         //нашим методам на вход даем список
-        if (/*1-й метод вернул true*/result) {
+        if (Methods.theFirstMethod(arrList)) { //либо у нас отработал 1-й метод и все хорошо
             result = true; //или false
-        } else {
+        } else if (/*2 метод*/result) { //либо у нас отработал 2-й метод и все хорошо
             //подаем начальный список на 2-й метод
             result = true; //или false
+        } else { //либо у нас не отработало ни то и ни другое
+            result = false;
         }
 
         return result;
