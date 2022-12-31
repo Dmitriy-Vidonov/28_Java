c2b6bf5dac022d52151f5b41b76c547194735be8
Dmitriy-Vidonov <73774560+Dmitriy-Vidonov@users.noreply.github.com>
2022-12-31 13:46:13 +0300

    refactor variables names

--- google_compet_7/src/main/java/Level1.java
+++ google_compet_7/src/main/java/Level1.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class Level1 {
     public static int[] WordSearch(int len, String s, String subs) {
-        List<String> spisok_strok = new ArrayList<>();
+        List<String> listOfStrings = new ArrayList<>();
         List<Integer> digits = new ArrayList<>();
 
         int count;
         while (s.length() > 0) {
             if(s.length() <= len) {
-                spisok_strok.add(s);
+                listOfStrings.add(s);
                 s = "";
             } else {
                 count = 0;
@@ -20,14 +20,14 @@ public class Level1 {
                 if(count > 0) {
                     for(int i = len; i > 0; i--) {
                         if(s.charAt(i) == ' ') {
-                            spisok_strok.add(s.substring(0, i));
+                            listOfStrings.add(s.substring(0, i));
                             s = s.substring((i + 1));
                             break;
                         }
                     }
                 } else {
                     for(int i = len; i > 0; i--) {
-                        spisok_strok.add(s.substring(0, i));
+                        listOfStrings.add(s.substring(0, i));
                         s = s.substring(i);
                         break;
                     }
@@ -35,15 +35,15 @@ public class Level1 {
             }
         }
 
-        for (int x=0; x<spisok_strok.size(); x++) {
-            if(spisok_strok.get(x).contains(subs) && subs.length() == spisok_strok.get(x).length()) {
+        for (int x=0; x<listOfStrings.size(); x++) {
+            if(listOfStrings.get(x).contains(subs) && subs.length() == listOfStrings.get(x).length()) {
                 digits.add(1);
-            } else if (spisok_strok.get(x).contains(' ' + subs + ' ')) {
+            } else if (listOfStrings.get(x).contains(' ' + subs + ' ')) {
                 digits.add(1);
-            } else if (subs.charAt(0) == spisok_strok.get(x).charAt(0) && spisok_strok.get(x).contains(subs + ' ')) {
+            } else if (subs.charAt(0) == listOfStrings.get(x).charAt(0) && listOfStrings.get(x).contains(subs + ' ')) {
                 digits.add(1);
-            } else if (subs.charAt(subs.length() - 1) == spisok_strok.get(x).charAt(spisok_strok.get(x).length() - 1)
-                    && spisok_strok.get(x).contains(' ' + subs)) {
+            } else if (subs.charAt(subs.length() - 1) == listOfStrings.get(x).charAt(listOfStrings.get(x).length() - 1)
+                    && listOfStrings.get(x).contains(' ' + subs)) {
                 digits.add(1);
             } else digits.add(0);
         }
